d254e487be233a6953d7eb45923729b0c903e82c
Evgeniy <antonm33@yandex.ru>
2020-10-25 09:33:10 +0200

    fix uncontrolled input

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -7,19 +7,18 @@ const Selector = ({ name, list, setQuery, isLoading }) => {
   const handleChange = (e) => {
     e.preventDefault();
     setValue(e.target.value);
-    setQuery((state) => ({ ...state, [e.target.name]: value }));
+    setQuery((state) => ({ ...state, [e.target.name]: e.target.value }));
   };
 
   return (
     <select
       name={name}
       className="form-select mx-4 mt-5 w-50"
-      aria-label="Default select example"
       disabled={isLoading}
       value={value}
       onChange={handleChange}
     >
-      <option value={name}>{name}</option>
+      <option value=''>{name}</option>
       {list.map((type, i) => (
         <option key={i} value={type}>
           {type}
@@ -35,5 +34,5 @@ Selector.propTypes = {
   name: PropTypes.string.isRequired,
   list: PropTypes.array.isRequired,
   setQuery: PropTypes.func.isRequired,
-  isLoadin: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
