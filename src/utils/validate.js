0735cd9865b9393be7d8414a6134f12b7b6f6fb0
EvgeniyPC <antonm33@yandex.ru>
2020-10-26 15:28:50 +0200

    add locastorage

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,8 +1,18 @@
+const saveUSerToLocalStorage = (user) => {
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+export const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('user'));
+};
+
 export const validateUser = (users, setUser) => (login, password) => {
   const user = users.find((u) => u.login === login);
   if (user) {
     if (user.password === password) {
       setUser((user) => ({ ...user, login, password, passCheck: true }));
+      const user = { login, password, passCheck: true };
+      saveUSerToLocalStorage(user);
       return true;
     }
   }
@@ -12,6 +22,9 @@ export const validateUser = (users, setUser) => (login, password) => {
 export const validateOtp = (otp, setUser) => (otpValue) => {
   if (otpValue === otp) {
     setUser((user) => ({ ...user, otpCheck: true }));
+    const user = getUserFromLocalStorage('user');
+    const userWithOtpCheck = { ...user, otpCheck: true };
+    saveUSerToLocalStorage(userWithOtpCheck);
     return true;
   }
   return false;
